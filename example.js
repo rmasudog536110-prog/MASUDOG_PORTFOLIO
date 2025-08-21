014bd846e0c4f1624c4d3952e94fa2f6076796e4
rmasudog536110-prog <r.masudog.536110@umindanao.edu.ph>
2025-08-21 19:30:50 -0300

    new changes

---
+++ example.js
@@ -0,0 +1,7 @@
+// Find the button by its ID
+const btn = document.getElementById("myBtn");
+
+// Add a click event
+btn.addEventListener("click", function() {
+  alert("Hello! 🎉 You just clicked the button.");
+});
